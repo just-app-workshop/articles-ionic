7689f48cb011d4c36ab484b8378bf7a49c168fd0
Christos Polychronis <euripas@gmail.com>
2016-04-24 14:25:37 +0300

    feat wordpress-service revamp

--- app/scripts/wordpress/wordpress.service.js
+++ app/scripts/wordpress/wordpress.service.js
@@ -10,8 +10,8 @@
 	/* @ngInject */
 	function wordpressService($http, $q) {
 		
-		var url = 'http://demo.titaniumtemplates.com/wordpress/?json=1';
-		var articles = [];
+		var url = 'http://demo.titaniumtemplates.com/wordpress/?json=1',
+		articles = [];
 		
 		var service = {
 			getArticles: getArticles,
@@ -20,56 +20,46 @@
 		return service;
 		
 		// *******************************************************
+		function handlePost(articles, item){
+			var imageUrl = item.attachments.length > 0 ? item.attachments[0].images.full.url : null;
+			var tags = [];
+			_.each(item.tags, function(tag) {
+				tags.push(tag.title);
+			});			
+			var contentIndex = item.content.indexOf('</p>') + 4;
+			var content = contentIndex === -1 ? item.content : item.content.substring(contentIndex);
+			articles.push({
+				id: item.id,
+				title: item.title,
+				brief: item.excerpt,
+				image: imageUrl,
+				date: item.date,
+				content: content,
+				author: item.author.name,
+				tags: tags,
+				url: url
+			});
+			return articles;
+		}
 		
-		// http://stackoverflow.com/questions/17533888/s3-access-control-allow-origin-header
 		function getArticles(articleId) {
-			var editedUrl = (!!articleId) ? url+'&p='+articleId: url;
-			return $http.get(editedUrl)
+			return $http.get(url)
 			.then(function(response) {
-				articles = [];
-				
-				_.each(response.data.posts, function(item) {
-					var imageUrl = item.attachments.length > 0 ? item.attachments[0].images.full.url : null;
-					var tags = [];
-					_.each(item.tags, function(tag) {
-						tags.push(tag.title);
-					});
-					
-					var contentIndex = item.content.indexOf('</p>') + 4;
-					var content = contentIndex === -1 ? item.content : item.content.substring(contentIndex);
-					
-					articles.push({
-						id: item.id,
-						title: item.title,
-						brief: item.excerpt,
-						image: imageUrl,
-						date: item.date,
-						content: content,
-						author: item.author.name,
-						tags: tags,
-						url: url
-					});
+				articles = [];				
+				_.each(response.data.posts, function (item){
+					articles = handlePost(articles, item);
 				});
 				return articles;
 			});
 		}		
 		
 		function getArticle(articleId) {
-			var tempArticle = getArticles(articleId);
-			if (tempArticle.length) {
-				return $q.when(_.find(tempArticle, 'id', articleId));
-				} else {
-				var deferred = $q.defer();
-				
-				getArticles()
-				.then(function() {
-					deferred.resolve(_.find(tempArticle, 'id', articleId));
-				});
-				
-				return deferred.promise;
-			}
+			return $http.get( url+'&p='+articleId)
+			.then(function(response) {
+				articles = [];	
+				articles = handlePost(articles, response.data.post)
+				return articles[0];
+			});
 		}
-		
-		
 	}
 })();
